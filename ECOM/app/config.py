51a1460b6d12dc018b829b35c8b992e771c7f5b0
Devam510 <pateldevam5354@gmail.com>
2025-10-31 20:33:05 +0530

    Update config.py

--- ECOM/app/config.py
+++ ECOM/app/config.py
@@ -5,8 +5,9 @@ load_dotenv()
 
 class Settings:
     DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/ecommerce")
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "your-secret-key-here")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "5d0254f31c923e14d56914bd09441ae31fad7892fbc562d53497773eb24b698f")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
 
 settings = Settings()
